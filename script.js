7c6381d48b17037aa476a36e29bed13f4c4c71b2
kirsrous <30956094+kirsrous@users.noreply.github.com>
2019-09-30 21:18:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+var playing = false;
+var score;
+var trialsLeft;
+var fruits = ['1','2','3','4','5','6','7','8'];
+var step;
+var action;
+$(function(){
+  $("#startreset").click(function() {
+    if(playing == true) {
+      location.reload();
+    } else {
+      playing = true;
+      score = 0;
+      $("#scorevalue").html(score);
+
+      $("#trialsleft").show();
+      trialsLeft = 3;
+      addHearts();
+
+      $("#gameover").hide();
+
+      $("#startreset").html("Reset Game");
+      startAction();
+    }
+
+  });
+
+  $("#fruit1").mouseover(function(){
+    score++;
+    $("#scorevalue").html(score);
+    $("#slicesound")[0].play();
+    clearInterval(action);
+    $("#fruit1").hide("explode",  500);
+    setTimeout(startAction, 500);
+  });
+
+  function addHearts() {
+    $("#trialsleft").empty();
+    for(i=0; i< trialsLeft; i++){
+      $("#trialsleft").append('<img src="images/heart.png" class="heart">');
+    }
+  }
+
+  function startAction() {
+    $("#fruit1").show();
+    choseFruit();
+    $("#fruit1").css({'left': Math.round(550 *Math.random()) , 'top': -50});
+
+    step =1 + Math.round(5 * Math.random());
+    action = setInterval(function(){
+      $("#fruit1").css('top', $("#fruit1").position().top + step);
+      if($("#fruit1").position().top > $("#fruitsContainer").height()) {
+        if(trialsLeft > 1){
+          $("#fruit1").show();
+          choseFruit();
+          $("#fruit1").css({'left': Math.round(550 *Math.random()) , 'top': -50});
+
+          step =1 + Math.round(5 * Math.random());
+          trialsLeft --;
+          addHearts();
+
+        } else {
+          playing = false;
+          $("#startreset").html("Start Game");
+          $("#gameover").show();
+          $("#gameover").html('<p>Game Over!</p><p>Your score is ' + score + ' </p>');
+          $("#trialsfeft").hide();
+          stopAction();
+        }
+      }
+    }, 10)
+  }
+
+  function choseFruit(){
+    var i = Math.round(Math.random() * 7)
+    $("#fruit1").attr('src', 'images/' + fruits[i] +  '.png')
+  }
+  function stopAction(){
+    clearInterval(action);
+    $("#fruit1").hide();
+  }
+
+});
